8a4f4d341d408d2cdd93c13ff85104010d6f96d3
rahat203 <rahatmpangarkar@gmail.com>
2023-08-07 20:56:57 +0530

    my life seventh commit

---
+++ views/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+const port = 8000;
+
+
+app.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname, '/about.html'));
+})
+
+
+app.get('/', 
+unction (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/contactpage', function (req, res) {
+    res.sendFile(path.join(__dirname, '/contactpage.html'));
+});
+
+
+app.listen(port, () => {
+    console.log("connected")
+})
+
+
+
+
+
+
